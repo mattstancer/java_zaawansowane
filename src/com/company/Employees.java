0893dd3d29ccb798b9f6812669c99caa7e44f626
mattstancer <mfjpoland@gmail.com>
2018-03-18 07:12:21 +0100

    jdbc with mysql

--- src/com/company/Employees.java
+++ src/com/company/Employees.java
@@ -33,16 +33,149 @@ public class Employees {
         return imie;
     }
 
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
 
     public String getNazwisko() {
         return nazwisko;
     }
 
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
+    public boolean DodajPracownika(){
+
+        try
+        {
+            setImie();
+            setNazwisko();
+            setWynagrodzenie();
+            setStanowisko();
+            setTelefon();
+        }
+        catch(Exception e){
+            System.out.println("Błąd podczas wprowadzania danych " + e.getMessage());
+            return false;
+        }
+
+        Wyswietl();
+        System.out.println("[Enter]\tZapisz");
+        System.out.println("[Q]\tPorzuć");
+
+        try{
+            while(true){
+                Scanner line = new Scanner(System.in);
+                char op = line.nextLine().charAt(0);
+                switch(op){
+                    case 'Q':
+                    case 'q':
+                        return false;
+                    default:
+                        break;
+                }
+            }
+        }
+        catch(Exception e){
+            return true;
+        }
+
+    }
+
+    Boolean setImie()
+    {
+        try{
+            System.out.println("Podaj Imie");
+            Scanner line = new Scanner(System.in);
+            String input = new String();
+            input = line.nextLine();
+            if(!input.isEmpty())
+                imie = input;
+            input = "";
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    Boolean setNazwisko()
+    {
+        try
+        {
+            System.out.println("Podaj Nazwisko");
+            Scanner line = new Scanner(System.in);
+            String input = new String();
+            input = line.nextLine();
+            if(!input.isEmpty())
+                nazwisko = input;
+            input = "";
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    Boolean setWynagrodzenie()
+    {
+        try
+        {
+            System.out.println("Podaj wynagrodzenie");
+            Scanner line = new Scanner(System.in);
+            String input = new String();
+            input = line.nextLine();
+            if(!input.isEmpty())
+                wynagrodzenie = new Integer(input);
+            input = "";
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    Boolean setStanowisko()
+    {
+        try
+        {
+            System.out.println("Podaj Stanowisko");
+            Scanner line = new Scanner(System.in);
+            String input = new String();
+            input = line.nextLine();
+            if(!input.isEmpty())
+                stanowisko = input;
+            input = "";
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    Boolean setTelefon()
+    {
+        try
+        {
+            System.out.println("Podaj Telefon");
+            Scanner line = new Scanner(System.in);
+            String input = new String();
+            input = line.nextLine();
+            if(!input.isEmpty())
+                telefon = input;
+            input = "";
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    public void Wyswietl() {
+
+        System.out.println("----------------------------------");
+        System.out.println("Identyfikator  : " + pesel.toString());
+        if(imie != null)
+            System.out.println("imie           : " + imie);
+        if(nazwisko != null)
+            System.out.println("Nazwisko       : " + nazwisko);
+        if(wynagrodzenie != null)
+            System.out.println("wynagrodzenie  : " + wynagrodzenie + "z�");
+        if(stanowisko != null)
+            System.out.println("Stanowisko     : " + stanowisko);
+        if(telefon != null)
+            System.out.println("Telefon        : " + telefon);
+
+        System.out.println("----------------------------------");
     }
 
     public BigDecimal getPayment() {
